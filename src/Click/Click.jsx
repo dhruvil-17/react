7aed6b9784b984894f3cd4c7b81f052d2ffdce53
dhruvil-17 <dhruvildodia91@gmail.com>
2025-06-28 10:57:06 +0530

    Clean repo by removing all files

--- src/Click/Click.jsx
+++
@@ -1,9 +0,0 @@
-function Click() {
-
-  return(<>
-    -
-  </>);
-
-}
-
-export default Click
--- src/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-function  Footer() {
-  return(
-    <h1>&copy; Footer</h1>
-  );
-}
-
-export default Footer
